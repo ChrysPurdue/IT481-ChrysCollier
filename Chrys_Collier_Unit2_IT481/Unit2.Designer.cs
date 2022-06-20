8ca89cc669d5c7f283050d1c1fdcdad683c0e8b5
ChrysPurdue <chryscollier@student.purdueglobal.edu>
2022-06-19 23:27:24 -0700

    Get customers and count rows.

--- Chrys_Collier_Unit2_IT481/Unit2.Designer.cs
+++ Chrys_Collier_Unit2_IT481/Unit2.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGetCustomers = new System.Windows.Forms.Button();
+            this.flpCustomers = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblCustomerCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetCustomers
+            // 
+            this.btnGetCustomers.AutoSize = true;
+            this.btnGetCustomers.Location = new System.Drawing.Point(13, 11);
+            this.btnGetCustomers.Name = "btnGetCustomers";
+            this.btnGetCustomers.Size = new System.Drawing.Size(86, 23);
+            this.btnGetCustomers.TabIndex = 0;
+            this.btnGetCustomers.Text = "Get Customers";
+            this.btnGetCustomers.UseVisualStyleBackColor = true;
+            this.btnGetCustomers.Click += new System.EventHandler(this.btnGetCustomers_Click);
+            // 
+            // flpCustomers
+            // 
+            this.flpCustomers.AutoScroll = true;
+            this.flpCustomers.Location = new System.Drawing.Point(13, 43);
+            this.flpCustomers.Name = "flpCustomers";
+            this.flpCustomers.Size = new System.Drawing.Size(414, 395);
+            this.flpCustomers.TabIndex = 1;
+            // 
+            // lblCustomerCount
+            // 
+            this.lblCustomerCount.AutoSize = true;
+            this.lblCustomerCount.Location = new System.Drawing.Point(106, 16);
+            this.lblCustomerCount.Name = "lblCustomerCount";
+            this.lblCustomerCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCustomerCount.TabIndex = 2;
+            this.lblCustomerCount.Visible = false;
+            // 
+            // Unit2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(439, 450);
+            this.Controls.Add(this.lblCustomerCount);
+            this.Controls.Add(this.flpCustomers);
+            this.Controls.Add(this.btnGetCustomers);
+            this.Name = "Unit2";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGetCustomers;
+        private System.Windows.Forms.FlowLayoutPanel flpCustomers;
+        private System.Windows.Forms.Label lblCustomerCount;
     }
 }
 
--- Chrys_Collier_Unit2_IT481/Unit2.cs
+++ Chrys_Collier_Unit2_IT481/Unit2.cs
@@ -16,7 +16,57 @@ namespace Chrys_Collier_Unit2_IT481
         public Unit2()
         {
             InitializeComponent();
-            //Data Source=DESKTOP-V2VF2CL;Initial Catalog=Northwind;Integrated Security=True
+        }
+        private void btnGetCustomers_Click(object sender, EventArgs e)
+        {
+            var count = 0;
+            foreach(String name in GetCustomers())
+            {
+                count += 1;
+                Label customerName = new Label();
+                customerName.Text = name;
+                customerName.Height = 50;
+                flpCustomers.Controls.Add(customerName);
+            }
+            lblCustomerCount.Text = "Results: "+count.ToString();
+            lblCustomerCount.Visible = true;
+            lblCustomerCount.Refresh();
+        }
+        private List<String> GetCustomers()
+        {
+            List<String> customers = new List<String>();
+            SqlConnection c;
+            c = CreateConnection();
+            string SelectCustomers =
+                "SELECT ContactName FROM Customers";
+            using (c)
+            {
+                SqlCommand cmd = new SqlCommand(SelectCustomers, c);
+                SqlDataReader r = cmd.ExecuteReader();
+                if(r.HasRows)
+                {
+                    while(r.Read())
+                    {
+                        string name = r.GetString(0);
+                        customers.Add(name);
+                    }
+                }
+            }
+            return customers;
+        }
+        private SqlConnection CreateConnection()
+        {
+            SqlConnection c;
+            c = new SqlConnection("Data Source=DESKTOP-V2VF2CL;Initial Catalog=Northwind;Integrated Security=True");
+            try
+            {
+                c.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return c;
         }
     }
 }
