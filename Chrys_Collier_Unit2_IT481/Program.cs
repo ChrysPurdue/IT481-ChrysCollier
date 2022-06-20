7bf06650f65b4670a7fe5d239f89c2cdf5cf93c8
chrys <chrys@DESKTOP-V2VF2CL>
2022-06-19 22:50:37 -0700

    Add project files.

---
+++ Chrys_Collier_Unit2_IT481/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chrys_Collier_Unit2_IT481
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Unit2());
+        }
+    }
+}
---
+++ Chrys_Collier_Unit2_IT481/Unit2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Chrys_Collier_Unit2_IT481
+{
+    public partial class Unit2 : Form
+    {
+        public Unit2()
+        {
+            InitializeComponent();
+            //Data Source=DESKTOP-V2VF2CL;Initial Catalog=Northwind;Integrated Security=True
+        }
+    }
+}
